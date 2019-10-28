fefbd7fec994e8248d0cda6af6965a678359aa05
Bogdan <orlovbogdan1998@gmail.com>
2019-10-28 18:01:47 +0200

    first commit

---
+++ les1/js/script.js
@@ -0,0 +1,20 @@
+var money = prompt("Ваш бюджет?");
+var nameShop = prompt("Название вашего магазина?");
+
+
+var mainList = {
+	cash: money,
+	name: nameShop,
+	//arr: shopGoods
+}
+
+
+/*
+
+var name1 = prompt('Какой тип товаров будем продавать?');
+var name2 = prompt('Какой тип товаров будем продавать?');
+var name3 = prompt('Какой тип товаров будем продавать?');
+var arr = [name1, name2, name3];
+
+*/
+alert(mainList.cash/30);
